91d2383597e3b418473724ca3ad1eeb1e4e642af
sulton0011 <azizxonkom@gmail.com>
2022-04-10 09:13:07 +0500

    Now only for post request

--- api/router.go
+++ api/router.go
@@ -5,6 +5,7 @@ import (
 	"ewallet/api/handlers"
 	"ewallet/config"
 	"ewallet/storage/repo"
+	"io"
 	"net/http"
 
 	"github.com/go-redis/redis/v8"
@@ -20,12 +21,18 @@ type Options struct {
 func New(options Options) {
 	handler := handlers.NewHandler(options.Cfg, options.Repo, options.Auth, options.Redis)
 
-	http.HandleFunc("/user/new", handler.AddUser)
-	http.HandleFunc("/wallet/new", handler.NewWallet)
+	http.Handle("/", http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		if r.Method == http.MethodPost {
+			http.HandleFunc("/user/new", handler.AddUser)
+			http.HandleFunc("/wallet/new", handler.NewWallet)
 
-	http.HandleFunc("/wallet/check/exist", handler.WalletCheckExists)
-	http.HandleFunc("/wallet/check/balance", handler.WalletCheckBalance)
-	http.HandleFunc("/wallet/fill", handler.WalletFill)
-	http.HandleFunc("/wallet/reduce", handler.WalletReduce)
-	http.HandleFunc("/wallet/history", handler.WalletHistory)
+			http.HandleFunc("/wallet/check/exist", handler.WalletCheckExists)
+			http.HandleFunc("/wallet/check/balance", handler.WalletCheckBalance)
+			http.HandleFunc("/wallet/fill", handler.WalletFill)
+			http.HandleFunc("/wallet/reduce", handler.WalletReduce)
+			http.HandleFunc("/wallet/history", handler.WalletHistory)
+		} else {
+			io.WriteString(w, "This is a " + r.Method + " request")
+		}
+	}))
 }
